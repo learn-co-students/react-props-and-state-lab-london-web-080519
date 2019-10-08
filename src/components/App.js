d48ba18f1ee0a5e8d4b37bb6ae5cce88957bb242
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-10-08 13:33:03 +0100

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,42 @@
-import React from 'react'
+import React from "react";
 
-import Filters from './Filters'
-import PetBrowser from './PetBrowser'
+import Filters from "./Filters";
+import PetBrowser from "./PetBrowser";
 
 class App extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       pets: [],
       filters: {
-        type: 'all'
-      }
-    }
+        type: "all",
+      },
+    };
   }
+
+  onChangeType = event => {
+    this.setState({ filters: { type: event.target.value } });
+  };
+
+  onFindPetsClick = () => {
+    this.state.filters.type === "all"
+      ? fetch("/api/pets")
+          .then(response => response.json())
+          .then(pets => this.setState({ pets: pets }))
+      : fetch(`/api/pets?type=${this.state.filters.type}`)
+          .then(response => response.json())
+          .then(pets => this.setState({ pets: pets }));
+  };
+
+  onAdoptPet = id => {
+    let petList = this.state.pets;
+    let adoptedPet = this.state.pets.find(item => item.id === id);
+    adoptedPet.isAdopted = true;
+    let index = this.state.pets.indexOf(adoptedPet);
+    petList[index] = adoptedPet;
+    this.setState(petList);
+  };
 
   render() {
     return (
@@ -24,16 +47,19 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+                onChangeType={this.onChangeType}
+                onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser pets={this.state.pets} onAdoptPet={this.onAdoptPet} />
             </div>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-export default App
+export default App;
